cd7d79db9f96eecbf8553000c56ee37f6253007c
Lucasoliveira1997 <lsdoliveira97@gmail.com>
2020-06-01 21:53:21 -0300

    refacting of route file, and create a model of centralize routes methods

---
+++ dist/common/model.router.js
@@ -0,0 +1,67 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const router_1 = require("./router");
+const mongoose = require("mongoose");
+const restify_errors_1 = require("restify-errors");
+class ModelRouter extends router_1.Router {
+    constructor(model) {
+        super();
+        this.model = model;
+        this.validateId = (req, resp, next) => {
+            if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+                next();
+            }
+            else {
+                next(new restify_errors_1.NotFoundError('Document Not Found'));
+            }
+        };
+        this.findAll = (req, resp, next) => {
+            this.model.find()
+                .then(this.renderAll(resp, next))
+                .catch(next);
+        };
+        this.findById = (req, resp, next) => {
+            this.model.findById(req.params.id)
+                .then(this.render(resp, next))
+                .catch(next);
+        };
+        this.save = (req, resp, next) => {
+            let document = new this.model(req.body);
+            document.save()
+                .then(this.render(resp, next))
+                .catch(next);
+        };
+        this.replace = (req, resp, next) => {
+            const options = { overwrite: true, runValidators: true };
+            this.model.update({ _id: req.params.id }, req.body, options)
+                .exec().then(result => {
+                if (result) {
+                    this.model.findById(req.params.id);
+                }
+                else {
+                    throw new restify_errors_1.NotFoundError('Document not found');
+                }
+            }).then(this.render(resp, next))
+                .catch(next);
+        };
+        this.update = (req, resp, next) => {
+            const options = { new: true, runValidators: true };
+            this.model.findByIdAndUpdate(req.params.id, req.body, options)
+                .then(this.render(resp, next))
+                .catch(next);
+        };
+        this.delete = (req, resp, next) => {
+            this.model.remove({ _id: req.params.id })
+                .exec().then((cmdResult) => {
+                if (cmdResult.result.n) {
+                    resp.send(204);
+                }
+                else {
+                    throw new restify_errors_1.NotFoundError('Document not found');
+                }
+                return next();
+            }).catch(next);
+        };
+    }
+}
+exports.ModelRouter = ModelRouter;
